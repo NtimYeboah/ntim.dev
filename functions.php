000da1ec5019b70bbab6af8fe8550417f389b25e
Ntim Yeboah <lylegendary@gmail.com>
2023-11-27 13:25:24 +0000

    Install giscus

--- functions.php
+++ functions.php
@@ -83,3 +83,29 @@ if (! function_exists('ntimdev_public_scripts')) {
     }
 }
 add_action('wp_enqueue_scripts', 'ntimdev_public_scripts');
+
+if (! function_exists('ntimdev_giscus_script')) {
+    function ntimdev_giscus_script() {
+        if (is_single()) {
+            ?>
+                <script src="https://giscus.app/client.js"
+                    data-repo="NtimYeboah/ntim.dev"
+                    data-repo-id="R_kgDOKwH1Gg"
+                    data-category="Announcements"
+                    data-category-id="DIC_kwDOKwH1Gs4CbHnQ"
+                    data-mapping="pathname"
+                    data-strict="0"
+                    data-reactions-enabled="1"
+                    data-emit-metadata="0"
+                    data-input-position="bottom"
+                    data-theme="preferred_color_scheme"
+                    data-lang="en"
+                    data-loading="lazy"
+                    crossorigin="anonymous"
+                    async>
+                </script>
+            <?php
+        }
+    }
+}
+add_action('wp_footer', 'ntimdev_giscus_script');
