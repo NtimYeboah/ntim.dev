1e84ef01eff3c1cc3dba8f3a0048c0b1930eef08
Ntim Yeboah <lylegendary@gmail.com>
2023-12-26 18:07:54 +0000

    Implement project page: Add real projects to page

--- template-parts/pages/content-projects.php
+++ template-parts/pages/content-projects.php
@@ -1,136 +1,113 @@
-<section class="md:w-6/12 md:mx-auto">
-    <div class="flex flex-col items-center space-y-10">
-        <div>
-            <h1 class="text-2xl font-extrabold dark:text-slate-100 mt-5">Projects</h1>
-        </div>
-        <div class="px-6">
-            <h2 class="text-lg text-slate-600 font-semibold text-center dark:text-slate-300">
-                I'm always working on projects and packages to make your life easier.<br>
-                Most of them are open-source, so you don't have to pay a dime!
-            </h2>
-        </div>
-        <div>
-            <div class="flex flex-col md:grid md:grid-cols-3 sm:grid sm:grid-cols-2">
-                <article class="p-5 hover:bg-slate-100 dark:hover:bg-slate-900">
+<div class="min-h-screen container sm:pl-4 sm:pr-4 md:mx-auto mt-5 mb-5">
+    <section id="primary-content" class="md:w-7/12 md:mx-auto flex flex-col">
+        <!-- Projects container -->
+        <div class="flex flex-col bg-slate-50 dark:bg-daccara">
+            <!-- Text container -->
+            <div class="flex flex-row gap-3">
+                <div>
+                    <div class="h-5 my-3 border-l-2 border-l-rose-500">
+                    </div>
+                </div>
+                <div>
+                    <div class="h-5 mt-2">
+                        <h2 class="font-saira font-bold text-xl dark:text-gray-400">Open Source</h2>
+                    </div>
+                </div>
+            </div>
+            <!-- End of Text container -->
+
+            <!-- Article container -->
+            <div class="flex flex-col md:grid md:grid-cols-2 md:gap-4 mx-4 mb-4">
+                <article class="dark:hover:bg-slate-900 mb-5 mt-3">
                     <a href="https://github.com/NtimYeboah/laravel-database-trigger">
-                        <picture class="mb-5 relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5">
-                            <img
-                                src="<?php echo esc_url(home_url() . '/wp-content/uploads/2023/11/github-120.png'); ?>"
-                                width="32"
-                                height="32"
-                                decoding="async"
-                                class="h-8 w-8 rounded-full"
-                                loading="lazy"
-                                style="color: transparent;"
-                                alt="">
-                        </picture>
-                        <h1 class="mb-3 font-extrabold text-gray-800 dark:text-slate-100">Laravel Database Trigger</h1>
-                        <p class="line-clamp-4 mb-5 dark:text-slate-400 text-gray-700">
-                            Laravel Database Trigger provides a way to add database trigger to laravel migrations just like you would with database table.
-                        </p>
-                        <div class="flex flex-row gap-2">
-                            <div class="bg-slate-50 rounded-sm pt-0 pb-1 px-1 dark:bg-cyan-900">
-                                <span class="dark:text-gray-200 text-gray-500 text-xs">Package</span>
+                        <div class="flex flex-col">
+                            <div class="flex flex-col gap-3">
+                                <picture class="relative z-10 flex w-12">
+                                    <img
+                                        src="<?php echo esc_url(home_url() . '/wp-content/uploads/2023/11/github-120.png'); ?>"
+                                        width="32"
+                                        height="32"
+                                        decoding="async"
+                                        class="h-8 w-8 rounded-full"
+                                        loading="lazy"
+                                        style="color: transparent;"
+                                        alt="">
+                                </picture>
+                                <h1 class="mb-2 font-sarabun font-bold text-gray-800 dark:text-slate-100 hover:underline">Laravel Database Trigger</h1>
+                            </div>
+                            
+                            <p class="line-clamp-4 mb-3 dark:text-slate-400 text-gray-700">
+                                Laravel Database Trigger provides a way to add database trigger to laravel migrations just like you would with database table.
+                            </p>
+                            <div>
+                                <p class="inline-flex text-gray-600 text-xs font-semibold">Package</p>
                             </div>
                         </div>
                     </a>
                 </article>
 
-                <article class="p-5 hover:bg-slate-100 dark:hover:bg-slate-900">
-                    <a href="https://github.com/NtimYeboah/laravel-database-trigger">
-                        <picture class="mb-5 relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5">
-                            <img
-                                src="<?php echo esc_url(home_url() . '/wp-content/uploads/2023/11/github-120.png'); ?>"
-                                width="32"
-                                height="32"
-                                decoding="async"
-                                class="h-8 w-8 rounded-full"
-                                loading="lazy"
-                                style="color: transparent;"
-                                alt="">
-                        </picture>
-                        <h1 class="mb-3 font-extrabold text-gray-800 dark:text-slate-100">Laravel Database Trigger</h1>
-                        <p class="line-clamp-4 mb-5 dark:text-slate-400 text-gray-700">
-                            Laravel Database Trigger provides a way to add database trigger to laravel migrations
-                            just like you would with database table.
-                        </p>
-                        <div class="flex flex-row gap-2">
-                            <div class="bg-slate-50 rounded-sm pt-0 pb-1 px-1 dark:bg-cyan-900">
-                                <span class="dark:text-gray-200 text-gray-500 text-xs">Package</span>
+                <article class="dark:hover:bg-slate-900 mb-5 mt-3">
+                    <a href="https://github.com/NtimYeboah/laravel-chatroom">
+                        <div class="flex flex-col">
+                            <div class="flex flex-col gap-3">
+                                <picture class="relative z-10 flex w-12">
+                                    <img
+                                        src="<?php echo esc_url(home_url() . '/wp-content/uploads/2023/11/github-120.png'); ?>"
+                                        width="32"
+                                        height="32"
+                                        decoding="async"
+                                        class="h-8 w-8 rounded-full"
+                                        loading="lazy"
+                                        style="color: transparent;"
+                                        alt="">
+                                </picture>
+                                <h1 class="mb-2 font-sarabun font-bold text-gray-800 dark:text-slate-100 hover:underline">Laravel Chatroom</h1>
+                            </div>
+                            
+                            <p class="line-clamp-4 mb-3 dark:text-slate-400 text-gray-700">
+                                This is a simple project to demonstrate how to build realtime applications using Laravel and Pusher or SocketIO.
+                                This is a chatting app that performs the following in real-time
+                            </p>
+                            <div>
+                                <p class="inline-flex text-gray-600 text-xs font-semibold">Tutorial</p>
                             </div>
                         </div>
                     </a>
                 </article>
 
-                <article class="p-5 hover:bg-slate-100 dark:hover:bg-slate-900">
-                    <a href="https://github.com/NtimYeboah/laravel-database-trigger">
-                        <picture class="mb-5 relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5">
-                            <img
-                                src="<?php echo esc_url(home_url() . '/wp-content/uploads/2023/11/github-120.png'); ?>"
-                                width="32"
-                                height="32"
-                                decoding="async"
-                                class="h-8 w-8 rounded-full"
-                                loading="lazy"
-                                style="color: transparent;"
-                                alt="">
-                        </picture>
-                        <h1 class="mb-3 font-extrabold text-gray-800 dark:text-slate-100">Laravel Database Trigger</h1>
-                        <p class="line-clamp-4 mb-5 dark:text-slate-400 text-gray-700">
-                            Laravel Database Trigger provides a way to add database trigger to laravel migrations
-                            just like you would with database table.
-                        </p>
-                        <div class="flex flex-row gap-2">
-                            <div class="bg-slate-50 rounded-sm pt-0 pb-1 px-1 dark:bg-cyan-900">
-                                <span class="dark:text-gray-200 text-gray-500 text-xs">Package</span>
+                <article class="dark:hover:bg-slate-900 mb-5 mt-3">
+                    <a href="https://github.com/NtimYeboah/learn-custom-authentication-in-laravel">
+                        <div class="flex flex-col">
+                            <div class="flex flex-col gap-3">
+                                <picture class="relative z-10 flex w-12">
+                                    <img
+                                        src="<?php echo esc_url(home_url() . '/wp-content/uploads/2023/11/github-120.png'); ?>"
+                                        width="32"
+                                        height="32"
+                                        decoding="async"
+                                        class="h-8 w-8 rounded-full"
+                                        loading="lazy"
+                                        style="color: transparent;"
+                                        alt="">
+                                </picture>
+                                <h1 class="mb-2 font-sarabun font-bold text-gray-800 dark:text-slate-100 hover:underline">Laravel Custom Authentication</h1>
+                            </div>
+                            
+                            <p class="line-clamp-4 mb-3 dark:text-slate-400 text-gray-700">
+                                Adding authentication to Laravel application is a matter running php artisan make:auth and the authentication routes and views will be added to you application. The authentication system will fetch the user to be authenticated from the database when using the default authentication scaffold.
+                            </p>
+                            <div>
+                                <p class="inline-flex text-gray-600 text-xs font-semibold">Tutorial</p>
                             </div>
                         </div>
                     </a>
                 </article>
 
-                <article class="p-5 hover:bg-slate-100 dark:hover:bg-slate-900">
-                    <a href="https://github.com/NtimYeboah/laravel-database-trigger">
-                        <picture class="mb-5 relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5">
-                            <img
-                                src="<?php echo esc_url(home_url() . '/wp-content/uploads/2023/11/github-120.png'); ?>"
-                                width="32"
-                                height="32"
-                                decoding="async"
-                                class="h-8 w-8 rounded-full"
-                                loading="lazy"
-                                style="color: transparent;"
-                                alt="">
-                        </picture>
-                        <h1 class="mb-3 font-extrabold text-gray-800 dark:text-slate-100">Laravel Database Trigger</h1>
-                        <p class="line-clamp-4 mb-5 dark:text-slate-400 text-gray-700">
-                            Laravel Database Trigger provides a way to add database trigger to laravel migrations
-                            just like you would with database table.
-                        </p>
-                        <div class="flex flex-row gap-2">
-                            <div class="bg-slate-50 rounded-sm pt-0 pb-1 px-1 dark:bg-cyan-900">
-                                <span class="dark:text-gray-200 text-gray-500 text-xs">Package</span>
-                            </div>
-                        </div>
-                    </a>
-                </article>
+                
+
+                
             </div>
         </div>
-    </div>
-</section>
-<section id="action" class="bg-cyan-900 p-5 mt-10">
-    <div class="md:w-9/12 md:mx-auto flex flex-col md:flex md:flex-row md:gap-3">
-        <div class="mb-5 md:basis-1/2">
-            <h4 class="font-semibold text-xl text-slate-200">Want to know about future blog posts?</h4>
-            <h4 class="font-semibold text-xl text-cyan-500">Then sign up for my newsletter!</h4>
-        </div>
-        <div class="mb-5 md:basis-1/2">
-            <div>
-                <input type="email" placeholder="Enter your email" class="w-3/4 p-2 rounded text-sm">
-                <button class=" bg-sky-700 text-white p-2 rounded shadow-md text-sm">Notify Me</button>
-            </div>
-            <div class="mt-3">
-                <p class="text-xs text-cyan-200">I'll only send mails if I actually have something to tell.</p>
-            </div>
-        </div>
-    </div>
-</section>
+        <!-- End of projects container -->
+    </section>
+</div>
