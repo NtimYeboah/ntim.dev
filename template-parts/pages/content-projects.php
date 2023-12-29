cb29956fb45b22927f4726e2a1c33dc29d4afb94
Ntim Yeboah <lylegendary@gmail.com>
2023-12-29 20:35:32 +0000

    Update styles on projects page

--- template-parts/pages/content-projects.php
+++ template-parts/pages/content-projects.php
@@ -1,7 +1,7 @@
-<div class="min-h-screen container sm:pl-4 sm:pr-4 md:mx-auto mt-5 mb-5">
-    <section id="primary-content" class="md:w-7/12 md:mx-auto flex flex-col">
+<div class="min-h-screen container px-4 sm:pl-4 sm:pr-4 md:mx-auto mt-5 mb-5">
+    <section class="md:w-7/12 md:mx-auto flex flex-col">
         <!-- Projects container -->
-        <div class="flex flex-col bg-slate-50 dark:bg-daccara">
+        <div class="flex flex-col bg-slate-50 dark:bg-gray-950">
             <!-- Text container -->
             <div class="flex flex-row gap-3">
                 <div>
@@ -10,7 +10,7 @@
                 </div>
                 <div>
                     <div class="h-5 mt-2">
-                        <h2 class="font-saira font-bold text-xl dark:text-gray-400">Open Source</h2>
+                        <h2 class="font-saira font-bold text-xl dark:text-gray-200">Open Source</h2>
                     </div>
                 </div>
             </div>
@@ -18,7 +18,7 @@
 
             <!-- Article container -->
             <div class="flex flex-col md:grid md:grid-cols-2 md:gap-4 mx-4 mb-4">
-                <article class="dark:hover:bg-slate-900 mb-5 mt-3">
+                <article class="mb-5 mt-3">
                     <a href="https://github.com/NtimYeboah/laravel-database-trigger">
                         <div class="flex flex-col">
                             <div class="flex flex-col gap-3">
@@ -28,25 +28,25 @@
                                         width="32"
                                         height="32"
                                         decoding="async"
-                                        class="h-8 w-8 rounded-full"
+                                        class="h-8 w-8 rounded-full dark:bg-black"
                                         loading="lazy"
                                         style="color: transparent;"
                                         alt="">
                                 </picture>
-                                <h1 class="mb-2 font-sarabun font-bold text-gray-800 dark:text-slate-100 hover:underline">Laravel Database Trigger</h1>
+                                <h1 class="mb-1 font-sarabun font-bold text-gray-800 dark:text-gray-300 hover:underline">Laravel Database Trigger</h1>
                             </div>
                             
-                            <p class="line-clamp-4 mb-3 dark:text-slate-400 text-gray-700">
+                            <p class="line-clamp-4 mb-3 dark:text-gray-300 text-gray-700">
                                 Laravel Database Trigger provides a way to add database trigger to laravel migrations just like you would with database table.
                             </p>
                             <div>
-                                <p class="inline-flex text-gray-600 text-xs font-semibold">Package</p>
+                                <p class="inline-flex text-gray-600 text-xs font-semibold dark:text-gray-400">Package</p>
                             </div>
                         </div>
                     </a>
                 </article>
 
-                <article class="dark:hover:bg-slate-900 mb-5 mt-3">
+                <article class="mb-5 mt-3">
                     <a href="https://github.com/NtimYeboah/laravel-chatroom">
                         <div class="flex flex-col">
                             <div class="flex flex-col gap-3">
@@ -56,26 +56,26 @@
                                         width="32"
                                         height="32"
                                         decoding="async"
-                                        class="h-8 w-8 rounded-full"
+                                        class="h-8 w-8 rounded-full dark:bg-black"
                                         loading="lazy"
                                         style="color: transparent;"
                                         alt="">
                                 </picture>
-                                <h1 class="mb-2 font-sarabun font-bold text-gray-800 dark:text-slate-100 hover:underline">Laravel Chatroom</h1>
+                                <h1 class="mb-1 font-sarabun font-bold text-gray-800 dark:text-gray-300 hover:underline">Laravel Database Trigger</h1>
                             </div>
                             
-                            <p class="line-clamp-4 mb-3 dark:text-slate-400 text-gray-700">
+                            <p class="line-clamp-4 mb-3 dark:text-gray-300 text-gray-700">
                                 This is a simple project to demonstrate how to build realtime applications using Laravel and Pusher or SocketIO.
                                 This is a chatting app that performs the following in real-time
                             </p>
                             <div>
-                                <p class="inline-flex text-gray-600 text-xs font-semibold">Tutorial</p>
+                                <p class="inline-flex text-gray-600 text-xs font-semibold dark:text-gray-400">Package</p>
                             </div>
                         </div>
                     </a>
                 </article>
 
-                <article class="dark:hover:bg-slate-900 mb-5 mt-3">
+                <article class="mb-5 mt-3">
                     <a href="https://github.com/NtimYeboah/learn-custom-authentication-in-laravel">
                         <div class="flex flex-col">
                             <div class="flex flex-col gap-3">
@@ -85,27 +85,23 @@
                                         width="32"
                                         height="32"
                                         decoding="async"
-                                        class="h-8 w-8 rounded-full"
+                                        class="h-8 w-8 rounded-full dark:bg-black"
                                         loading="lazy"
                                         style="color: transparent;"
                                         alt="">
                                 </picture>
-                                <h1 class="mb-2 font-sarabun font-bold text-gray-800 dark:text-slate-100 hover:underline">Laravel Custom Authentication</h1>
+                                <h1 class="mb-1 font-sarabun font-bold text-gray-800 dark:text-gray-300 hover:underline">Laravel Database Trigger</h1>
                             </div>
                             
-                            <p class="line-clamp-4 mb-3 dark:text-slate-400 text-gray-700">
+                            <p class="line-clamp-4 mb-3 dark:text-gray-300 text-gray-700">
                                 Adding authentication to Laravel application is a matter running php artisan make:auth and the authentication routes and views will be added to you application. The authentication system will fetch the user to be authenticated from the database when using the default authentication scaffold.
                             </p>
                             <div>
-                                <p class="inline-flex text-gray-600 text-xs font-semibold">Tutorial</p>
+                                <p class="inline-flex text-gray-600 text-xs font-semibold dark:text-gray-400">Package</p>
                             </div>
                         </div>
                     </a>
                 </article>
-
-                
-
-                
             </div>
         </div>
         <!-- End of projects container -->
