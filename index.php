6271edab893aa87cf46388c57d6bd60971d83907
Ntim Yeboah <lylegendary@gmail.com>
2023-12-29 19:26:52 +0000

    Update homepage styles

---
+++ index.php
@@ -0,0 +1,114 @@
+<?php
+/*
+ * Main template file
+ */
+get_header();
+?>
+
+<?php
+$primary_posts = new WP_Query([
+    'tag' => 'article',
+    'posts_per_page' => 4
+]);
+
+if ($primary_posts->have_posts()):
+?>
+
+<div class="container px-4 sm:pl-4 sm:pr-4 md:mx-auto mt-5">
+    <section id="primary-content" class="md:w-7/12 md:mx-auto flex flex-col">
+        <!-- Lastest posts container -->
+        <div class="flex flex-col bg-slate-50 dark:bg-gray-950">
+            <!-- Text container -->
+            <div class="flex flex-row gap-3">
+                <div>
+                    <div class="h-5 my-3 border-l-2 border-l-rose-500">
+                    </div>
+                </div>
+                <div>
+                    <div class="h-5 mt-2">
+                        <h2 class="font-saira font-bold text-xl dark:text-gray-200">Latest Posts</h2>
+                    </div>
+                </div>
+            </div>
+            <!-- End of Text container -->
+
+            <!-- Article container -->
+            <div class="flex flex-col md:grid md:grid-cols-2 md:gap-4 mx-4 mb-4">
+                <?php
+                    while ($primary_posts->have_posts()):
+                        $primary_posts->the_post();
+                        get_template_part('template-parts/posts/content');
+                    endwhile;
+
+                    wp_reset_postdata();
+                ?>
+            </div>
+        </div>
+        <!-- End of Latest posts container -->
+    </section>
+</div>
+<?php else: ?>
+    <section class="md:w-6/12 md:mx-auto container" id="primary-content">
+        <header class="my-4">
+            <div class="flex justify-center">
+                <h1 class="font-extrabold dark:text-slate-100">No posts</h1>
+            </div>
+        </header>
+    </section>
+<?php endif; ?>
+
+<?php
+$tips_query = new WP_Query([
+    'tag' => 'tips',
+    'posts_per_page' => 4
+]);
+
+if ($tips_query->have_posts()):
+?>
+<div class="container px-4 mb-10 md:mx-auto my-5">
+    <section id="tips-and-snippets" class="md:w-7/12 md:mx-auto flex flex-col">
+        <!--  Tips container -->
+        <div class="bg-slate-50 dark:bg-gray-950">
+            <!-- Text container -->
+            <div class="flex flex-row gap-3">
+                <div class="">
+                    <div class="h-5 my-3 border-l-2 border-l-rose-500">
+                    </div>
+                </div>
+                <div>
+                    <div class="h-5 mt-2">
+                    <h2 class="font-saira font-bold text-xl dark:text-gray-200">Tips & Snippets</h2>
+                    </div>
+                </div>
+            </div>
+            <!-- End of Text Container -->
+
+            <!-- Article container -->
+            <div class="flex flex-col md:grid md:grid-cols-2 pb-5 px-4 gap-3">
+                <?php
+                while ($tips_query->have_posts()):
+                    $tips_query->the_post();
+                ?>
+                <article class="mb-5">
+                    <a href="<?php echo esc_url(get_permalink()); ?>">
+                        <div class="flex flex-row">
+                            <div class="w-12/12">
+                                <?php the_title('<h3 class="hover:underline font-sarabun font-bold mb-2 dark:text-gray-300">', '</h3>'); ?>
+                                <p class="line-clamp-2 dark:text-gray-300"><?php the_excerpt(); ?></p>
+                            </div>
+                        </div>
+                    </a>
+                </article>
+                <?php
+                endwhile;
+
+                wp_reset_postdata();
+                ?>
+            </div>
+        </div>
+    </section>
+</div>
+<?php endif; ?>
+
+<?php
+get_footer();
--- template-parts/posts/content.php
+++ template-parts/posts/content.php
@@ -4,7 +4,7 @@
  */
 ?>
 
-<article class="hover:bg-slate-100 dark:hover:bg-faccara">
+<article class="dark:hover:bg-slate-900 hover:bg-slate-100 mb-4">
     <a id="post-<?php the_ID(); ?>" href="<?php echo esc_url(get_permalink()); ?>">
         <div class="flex-col">
             <?php if (has_post_thumbnail()): ?>
@@ -15,21 +15,21 @@
         </div>
         <div class="flex flex-col px-4">
             <div class="flex flex-col">
-                <?php the_title('<h3 class="font-sarabun mt-2 font-extrabold text-lg text-left dark:text-gray-400 tracking-wide">', '</h3>'); ?>
+                <?php the_title('<h3 class="font-sarabun mt-5 font-extrabold text-lg text-left dark:text-gray-300 tracking-wide">', '</h3>'); ?>
 
-                <p class="text-base text-gray-900 dark:text-gray-400 line-clamp-4 mt-5"><?php the_excerpt(); ?></p>
+                <p class="text-base text-gray-900 dark:text-gray-300 line-clamp-4 mt-5"><?php the_excerpt(); ?></p>
             </div>
             <div class="flex flex-row py-4 gap-2 justify-left">
                 <div>
-                    <p class="inline-flex text-gray-600 text-xs font-semibold"><?php echo the_time('m.d.Y'); ?></p>
+                <p class="inline-flex text-gray-600 text-xs font-semibold dark:text-gray-400"><?php echo the_time('m.d.Y'); ?></p>
                 </div>
                 <?php $cats = get_the_category(); ?>
                 <?php if (count($cats) > 0): ?>
                     <div>
-                        <span class="font-semibold text-gray-500">|</span>
+                    <span class="font-semibold text-gray-500 dark:text-gray-400">|</span>
                     </div>
                     <div>
-                        <p class="inline-flex text-gray-600 text-xs font-semibold"><?php echo $cats[0]->name; ?></p>
+                    <p class="inline-flex text-gray-600 text-xs font-semibold dark:text-gray-400"><?php echo $cats[0]->name; ?></p>
                     </div>
                 <?php endif; ?>
             </div>
