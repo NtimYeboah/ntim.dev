f83fcd4e233a649c23b014670234eb2ff9675ecb
Ntim Yeboah <lylegendary@gmail.com>
2024-02-08 07:59:39 +0000

    Dynamically show featured post

--- index.php
+++ index.php
@@ -5,15 +5,6 @@
 get_header();
 ?>
 
-<?php
-$primary_posts = new WP_Query([
-    'tag' => 'article',
-    'posts_per_page' => 4
-]);
-
-if ($primary_posts->have_posts()):
-?>
-
 <div class="container flex flex-col px-4 mx-auto md:w-8/12 mb-5 mt-5">
     <div class="flex flex-col">
         <div class="flex flex-col md:w-8/12">
@@ -60,6 +51,20 @@ if ($primary_posts->have_posts()):
             <!-- End of search for sm screens -->
         </div>
     </div>
+    <?php
+    $args = [
+        'tax_query' => [
+            [
+                'taxonomy' => 'post_tag',
+                'field' => 'slug',
+                'terms' => ['featured']
+            ]
+        ]
+    ];
+    $featured_article = new WP_Query($args);
+
+    if ($featured_article):
+    ?>
     <div class="flex flex-col">
         <!-- Featured Article -->
         <div class="flex flex-col mb-5 bg-slate-50 dark:bg-gray-950">
@@ -74,33 +79,31 @@ if ($primary_posts->have_posts()):
                     </div>
                 </div>
             </div>
-
-            <div class="hidden flex-row md:grid md:grid-cols-2 dark:hover:bg-slate-900 hover:bg-slate-100 md:gap-4 mx-4 mb-4 bg-50% bg-no-repeat bg-right" style="background-image: url('./assets/img/clock-unsplash.jpg')">
+            <?php $featured_article->the_post(); ?>
+            <div class="hidden flex-row md:grid md:grid-cols-2 dark:hover:bg-slate-900 hover:bg-slate-100 md:gap-4 mx-4 mb-4 bg-50% bg-no-repeat bg-right" style="background-image: url('<?php echo the_post_thumbnail_url(); ?>')">
                 <article class="dark:hover:bg-slate-900 hover:bg-slate-100 mb-4">
-                    <a href="/page-details.html" class="">
+                    <a id="feature-article-<?php the_ID(); ?>" href="<?php echo esc_url(get_permalink()); ?>" class="">
                         <div class="flex flex-col px-4">
                             <div class="flex flex-col">
-                                <h3 class="font-sarabun mt-5 font-extrabold text-2xl text-left dark:text-gray-300 tracking-wide">
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                </h3>
+                                <?php the_title('<h3 class="font-sarabun mt-5 font-extrabold text-2xl text-left dark:text-gray-300 tracking-wide">', '</h3>'); ?>
+
                                 <p class="text-lg text-gray-900 dark:text-gray-300 line-clamp-5 mt-5">
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                    <?php the_excerpt(); ?>
                                 </p>
                             </div>
                             <div class="flex flex-row py-4 gap-2 justify-left">
                                 <div>
-                                    <p class="inline-flex text-gray-600 text-sm font-semibold dark:text-gray-400">1.1.2024</p>
+                                    <p class="inline-flex text-gray-600 text-sm font-semibold dark:text-gray-400"><?php echo the_time('m.d.Y'); ?></p>
                                 </div>
+                                <?php $cats = get_the_category(); ?>
+                                <?php if (count($cats) > 0): ?>
                                 <div>
                                     <span class="font-semibold text-gray-500 dark:text-gray-400">|</span>
                                 </div>
                                 <div>
-                                    <p class="inline-flex text-gray-600 text-sm font-semibold dark:text-gray-400 uppercase">Volutpat</p>
+                                    <p class="inline-flex text-gray-600 text-sm font-semibold dark:text-gray-400 uppercase"><?php echo $cats[0]->name; ?></p>
                                 </div>
+                                <?php endif; ?>
                             </div>
                         </div>
                     </a>
@@ -109,34 +112,33 @@ if ($primary_posts->have_posts()):
 
             <div class="md:hidden flex flex-col mx-4 mb-2">
                 <article class="dark:hover:bg-slate-900 hover:bg-slate-100 mb-4">
-                    <a href="/page-details.html" class="">
+                    <a href="<?php echo esc_url(get_permalink()); ?>" class="">
                         <div class="flex-col">
                             <picture>
-                                <img class="w-full h-48 object-cover object-center mb-2" src="/assets/img/clock-unsplash.jpg" alt="">
+                                <img class="w-full h-48 object-cover object-center mb-2" src="<?php echo the_post_thumbnail_url(); ?>" alt="">
                             </picture>
                         </div>
                         <div class="flex flex-col px-4">
                             <div class="flex flex-col">
-                                <h3 class="font-sarabun mt-5 font-extrabold text-lg text-left dark:text-gray-300 tracking-wide">
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                </h3>
+                                <?php the_title('<h3 class="font-sarabun mt-5 font-extrabold text-lg text-left dark:text-gray-300 tracking-wide">', '</h3>'); ?>
 
                                 <p class="text-base text-gray-900 dark:text-gray-300 line-clamp-4 mt-5">
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                    <?php the_excerpt(); ?>
                                 </p>
                             </div>
                             <div class="flex flex-row py-4 gap-2 justify-left">
                                 <div>
-                                    <p class="inline-flex text-gray-600 text-xs font-semibold dark:text-gray-400">1.1.2024</p>
+                                    <p class="inline-flex text-gray-600 text-xs font-semibold dark:text-gray-400"><?php echo the_time('m.d.Y'); ?></p>
                                 </div>
+                                <?php $cats = get_the_category(); ?>
+                                <?php if (count($cats) > 0): ?>
                                 <div>
                                     <span class="font-semibold text-gray-500 dark:text-gray-400">|</span>
                                 </div>
                                 <div>
-                                    <p class="inline-flex text-gray-600 text-xs font-semibold dark:text-gray-400 uppercase">Volutpat</p>
+                                    <p class="inline-flex text-gray-600 text-xs font-semibold dark:text-gray-400 uppercase"><?php echo $cats[0]->name; ?></p>
                                 </div>
+                                <?php endif; ?>
                             </div>
                         </div>
                     </a>
@@ -145,6 +147,16 @@ if ($primary_posts->have_posts()):
         </div>
         <!-- End of featured article -->
     </div>
+    <?php endif; ?>
+
+    <?php
+    $primary_posts = new WP_Query([
+        'tag' => 'article',
+        'posts_per_page' => 4
+    ]);
+
+    if ($primary_posts->have_posts()):
+    ?>
     <div class="flex flex-col md:flex-row gap-4">
         <div class="flex flex-col md:w-8/12">
 
